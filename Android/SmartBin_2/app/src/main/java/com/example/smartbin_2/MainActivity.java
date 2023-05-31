b76094ff29f5d8a57c25bf97c24ff2e9f647efd2
Maximiliano Leonel Morales <62450950+mmaxi94@users.noreply.github.com>
2023-05-31 01:37:19 -0300

    se agrega proyecto arduino

---
+++ Android/SmartBin_2/app/src/main/java/com/example/smartbin_2/MainActivity.java
@@ -0,0 +1,372 @@
+package com.example.smartbin_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private ImageView ivLogo, ivConexion;
+    private Button btnConectar;
+    private String estado = "desconectado";
+    private BluetoothAdapter mBluetoothAdapter;
+    private ProgressDialog mProgressDlg;
+    public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
+
+    private ArrayList<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
+
+    String[] permissions= new String[]{
+            android.Manifest.permission.BLUETOOTH,
+            android.Manifest.permission.BLUETOOTH_ADMIN,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivLogo = findViewById(R.id.ivLogo);
+        ivConexion = findViewById(R.id.ivConexion);
+        btnConectar = (Button) findViewById(R.id.btnConectar);
+
+        // Accedemos al servicio de sensores
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //Se crea un adaptador para podermanejar el bluethoot del celular
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //Se Crea la ventana de dialogo que indica que se esta buscando dispositivos bluethoot
+        mProgressDlg = new ProgressDialog(this);
+
+        mProgressDlg.setMessage("Buscando dispositivos...");
+        mProgressDlg.setCancelable(false);
+
+        //se asocia un listener al boton cancelar para la ventana de dialogo ue busca los dispositivos bluethoot
+        mProgressDlg.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancelar", btnCancelarDialogListener);
+
+
+        if (checkPermissions())
+        {
+            enableComponent();
+        }
+    }
+
+    //Metodo que actua como Listener de los eventos que ocurren en los componentes graficos de la activty
+    private View.OnClickListener btnEmparejarListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices == null || pairedDevices.size() == 0)
+            {
+                Toast.makeText(getApplicationContext(), "No se encontraron dispositivos emparejados", Toast.LENGTH_SHORT).show();
+
+            }
+            else
+            {
+                ArrayList<BluetoothDevice> list = new ArrayList<BluetoothDevice>();
+
+                list.addAll(pairedDevices);
+
+                Intent intent = new Intent(MainActivity.this, DeviceListActivity.class);
+
+                intent.putParcelableArrayListExtra("device.list", list);
+
+                startActivity(intent);
+            }
+        }
+    };
+
+    protected  void enableComponent()
+    {
+        //se determina si existe bluethoot en el celular
+        if (mBluetoothAdapter == null)
+        {
+            //si el celular no soporta bluethoot
+        }
+        else
+        {
+            //si el celular soporta bluethoot, se definen los listener para los botones de la activity
+            btnConectar.setOnClickListener(btnEmparejarListener);
+
+
+            //se determina si esta activado el bluethoot
+            if (mBluetoothAdapter.isEnabled())
+            {
+                //se informa si esta habilitado
+
+            }
+            else
+            {
+                //se informa si esta deshabilitado
+
+            }
+        }
+
+
+        //se definen un broadcastReceiver que captura el broadcast del SO cuando captura los siguientes eventos:
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED); //Cambia el estado del Bluethoot (Acrtivado /Desactivado)
+        filter.addAction(BluetoothDevice.ACTION_FOUND); //Se encuentra un dispositivo bluethoot al realizar una busqueda
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED); //Cuando se comienza una busqueda de bluethoot
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED); //cuando la busqueda de bluethoot finaliza
+
+        //se define (registra) el handler que captura los broadcast anterirmente mencionados.
+        registerReceiver(mReceiver, filter);
+    }
+
+    //Handler que captura los brodacast que emite el SO al ocurrir los eventos del bluethoot
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+
+            //Atraves del Intent obtengo el evento de Bluethoot que informo el broadcast del SO
+            String action = intent.getAction();
+
+            //Si cambio de estado el Bluethoot(Activado/desactivado)
+            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action))
+            {
+                //Obtengo el parametro, aplicando un Bundle, que me indica el estado del Bluethoot
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+
+                //Si esta activado
+                if (state == BluetoothAdapter.STATE_ON)
+                {
+                    Toast.makeText(getApplicationContext(), "Activar", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            //Si se inicio la busqueda de dispositivos bluethoot
+            else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
+            {
+                //Creo la lista donde voy a mostrar los dispositivos encontrados
+                mDeviceList = new ArrayList<BluetoothDevice>();
+
+                //muestro el cuadro de dialogo de busqueda
+                mProgressDlg.show();
+            }
+            //Si finalizo la busqueda de dispositivos bluethoot
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+                //se cierra el cuadro de dialogo de busqueda
+                mProgressDlg.dismiss();
+
+                //se inicia el activity DeviceListActivity pasandole como parametros, por intent,
+                //el listado de dispositovos encontrados
+                Intent newIntent = new Intent(MainActivity.this, DeviceListActivity.class);
+
+                newIntent.putParcelableArrayListExtra("device.list", mDeviceList);
+
+                startActivity(newIntent);
+            }
+            //si se encontro un dispositivo bluethoot
+            else if (BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                //Se lo agregan sus datos a una lista de dispositivos encontrados
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                mDeviceList.add(device);
+                Toast.makeText(getApplicationContext(), "Dispositivo Encontrado:"+ device.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private DialogInterface.OnClickListener btnCancelarDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+
+            mBluetoothAdapter.cancelDiscovery();
+        }
+    };
+
+    //Metodo que chequea si estan habilitados los permisos
+    private  boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        //Se chequea si la version de Android es menor a la 6
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+
+
+        for (String p:permissions) {
+            result = ContextCompat.checkSelfPermission(this,p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),MULTIPLE_PERMISSIONS );
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permissions granted.
+                    enableComponent(); // Now you call here what ever you want :)
+                } else {
+                    String perStr = "";
+                    for (String per : permissions) {
+                        perStr += "\n" + per;
+                    }
+                    // permissions list of don't granted permission
+                    Toast.makeText(this, "ATENCION: La aplicacion no funcionara " +
+                            "correctamente debido a la falta de Permisos", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+
+/*
+    public void presionarConectarSmartBin(View v){
+
+        if (estado.equals("desconectado")) {
+            ivConexion.setImageResource(R.drawable.connected_icon);
+            btnConectar.setText("DESCONECTARSE DE SMARTNBIN");
+            Toast.makeText(this, "Conectado", Toast.LENGTH_SHORT).show();
+            estado = "conectado";
+        }
+        else {
+            ivConexion.setImageResource(R.drawable.disconnected_icon);
+            btnConectar.setText("CONECTARSE A SMARTNBIN");
+            Toast.makeText(this, "Desconectado", Toast.LENGTH_SHORT).show();
+            estado = "desconectado";
+        }
+
+
+    }*/
+
+    // Metodo para iniciar el acceso a los sensores
+    protected void Ini_Sensores()
+    {
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),   SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    // Metodo para parar la escucha de los sensores
+    private void Parar_Sensores()
+    {
+
+        mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    // Metodo que escucha el cambio de sensibilidad de los sensores
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+
+    // Metodo que escucha el cambio de los sensores
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String txt = "";
+
+        // Cada sensor puede lanzar un thread que pase por aqui
+        // Para asegurarnos ante los accesos simult�neos sincronizamos esto
+
+        synchronized (this) {
+            Log.d("sensor", event.sensor.getName());
+
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    if ((event.values[0] > 15) || (event.values[1] > 15) || (event.values[2] > 15)) {
+                        Toast.makeText(this, "Vibracion Detectada", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+
+            }
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+
+        Parar_Sensores();
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        Parar_Sensores();
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        Parar_Sensores();
+        if (mBluetoothAdapter != null) {
+            if (mBluetoothAdapter.isDiscovering()) {
+                mBluetoothAdapter.cancelDiscovery();
+            }
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        Ini_Sensores();
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        Ini_Sensores();
+    }
+
+}
