812bb2480835c004b1fa29f62034cbc7f624f267
Maxi Morales <maximorales94@yahoo.com.ar>
2023-06-20 14:51:30 -0300

    se agrega nuevo proyecto de arduino

--- Android/SmartBin_2020_2/app/src/main/java/com/example/smartbin_2020_2/Mantenimiento.java
+++ Android/SmartBin_2020_2/app/src/main/java/com/example/smartbin_2020_2/Mantenimiento.java
@@ -2,13 +2,32 @@ package com.example.smartbin_2020_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mantenimiento extends AppCompatActivity {
+
+    private Button btcbuttonMan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mantenimiento);
+
+
+        btcbuttonMan = (Button) findViewById(R.id.buttonMan);
+
+
+        btcbuttonMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BTHandler.getInstance().sendMsg(new Message(Command.DETENER_RIEGO));
+            }
+        });
     }
+
+
 }
